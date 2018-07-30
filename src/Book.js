03ff3c0ffc71e14f5e9e6ac4a6686413674e55bf
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-07-30 13:07:10 +0200

    Created and imported Book.js component

--- src/Book.js
+++ src/Book.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
+import BookShelfChanger from './BookShelfChanger'
 
 export class Book extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+        <React.Fragment>
+            <li>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
+                  <BookShelfChanger/>
+                </div>
+                <div className="book-title">1776</div>
+                <div className="book-authors">David McCullough</div>
+              </div>
+            </li>
+        </React.Fragment>
     )
   }
 }
 
-export default Book
+export default Book;
