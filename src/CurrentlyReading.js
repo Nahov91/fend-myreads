b215cac28a7fdd08a49f00bcbbea3ac1b03bb8a8
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-07-30 12:07:17 +0200

    Setting up components

---
+++ src/CurrentlyReading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CurrentlyReading extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CurrentlyReading
