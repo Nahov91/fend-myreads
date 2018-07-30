c2cb7442286b2c222b489791f90c46ee91627cef
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-07-30 14:35:49 +0200

    Books now have author and title props

--- src/Book.js
+++ src/Book.js
@@ -3,6 +3,7 @@ import BookShelfChanger from './BookShelfChanger'
 
 export class Book extends Component {
   render() {
+      const { title,author} = this.props
     return (
         <React.Fragment>
             <li>
@@ -11,8 +12,8 @@ export class Book extends Component {
                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
                   <BookShelfChanger/>
                 </div>
-                <div className="book-title">1776</div>
-                <div className="book-authors">David McCullough</div>
+                <div className="book-title">{title}</div>
+                <div className="book-authors">{author}</div>
               </div>
             </li>
         </React.Fragment>
