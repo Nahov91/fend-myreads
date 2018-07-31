22516066547bb8e50f9293cab8010349e9c7c649
Nahov91 <35198701+Nahov91@users.noreply.github.com>
2018-07-31 11:22:59 +0200

    Search field now takes a query

--- src/Search.js
+++ src/Search.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
+import escapeRegExp from 'escape-string-regexp'
 
 class Search extends React.Component {
-    render(){
+  state={query:''}
+  searchQuery= (query)=> {
+    this.setState({query})
+  }
+    render(){   
         return(
             <div className="search-books">
               <div className="search-books-bar">
                 <Link className="close-search" to='/'>Close</Link>
                 <div className="search-books-input-wrapper">
-                  {/*
-                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                    You can find these search terms here:
-                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-  
-                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                    you don't find a specific author or title. Every search is limited by search terms.
-                  */}
-                  <input type="text" placeholder="Search by title or author"/>
+                  {JSON.stringify(this.state)}
+                  <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(e)=> this.searchQuery(e.target.value)}/>
   
                 </div>
               </div>
